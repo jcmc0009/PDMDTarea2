e84e3ebcb2e24e290f827c4817f3e2fc230e96b0
jcmc0009 <jcmc0009@gmail.com>
2024-12-07 18:43:56 +0100

    Initial commit

--- app/src/main/java/com/example/tarea2/Aplication.java
+++ app/src/main/java/com/example/tarea2/Aplication.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Aplicacion extends AppCompatActivity {
+public class Aplication extends AppCompatActivity {
 /**
  * */
     @Override
--- app/src/main/java/com/example/tarea2/MainActivity.java
+++ app/src/main/java/com/example/tarea2/MainActivity.java
@@ -11,9 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    /**Creamos una variable en la que el número será el tiempo de espera de la imagen splash
-     * */
-public static int tiempoCarga=2500;
+    /**
+     * Creamos una variable en la que el número será el tiempo de espera de la imagen splash
+     */
+    public static int tiempoCarga = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +26,21 @@ public static int tiempoCarga=2500;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+/**
+ * * Handler para retrasar la ejecución de una tarea. */
+
         new Handler().postDelayed(new Runnable() {
             @Override
+/**
+ * * Método que se ejecuta después del retraso. */
             public void run() {
-                Intent intent =new Intent(MainActivity.this, Aplicacion.class);
+                /**
+                 * * Inicia la actividad Aplication
+                 * * * @param intent Intent para iniciar la nueva actividad. */
+                Intent intent = new Intent(MainActivity.this, Aplication.class);
                 startActivity(intent);
                 finish();
             }
-        },tiempoCarga);
+        }, tiempoCarga);
     }
 }
