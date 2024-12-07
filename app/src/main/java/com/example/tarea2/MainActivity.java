f9fd720a4771f8df71bf7bf127b42e6c868287ae
jcmc0009 <jcmc0009@gmail.com>
2024-12-07 02:08:21 +0100

    Initial commit

--- app/src/main/java/com/example/tarea2/MainActivity.java
+++ app/src/main/java/com/example/tarea2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tarea2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**Creamos una variable en la que el número será el tiempo de espera de la imagen splash
+     * */
+public static int tiempoCarga=2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(MainActivity.this, Aplicacion.class);
+                startActivity(intent);
+                finish();
+            }
+        },tiempoCarga);
     }
 }
